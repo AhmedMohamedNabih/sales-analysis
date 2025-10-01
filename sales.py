2cf0ca0a7014da141f48b1612716210b56a88db4
AhmedMohamedNabih <am5313634@gmail.com>
2025-10-01 15:12:13 +0300

    Update sales.py

--- sales.py
+++ sales.py
@@ -3,7 +3,7 @@ import streamlit as st
 import plotly.express as px 
 import numpy as np
 st.set_page_config(layout='wide',page_title='Sales EDA')
-df=pd.read_csv('E:/preprocessing 8/cleaning_df.csv',index_col=0)
+df = pd.read_csv('cleaning_df.csv', index_col=0)
 st.title('what is the precentage of each city in the Data ?')
 st.plotly_chart(px.pie(df,names='city'))
 rev_city=df.groupby('city')['Price Each'].sum().sort_values(ascending=False).reset_index()
@@ -13,3 +13,4 @@ df_sorted=df.sort_values(by='Order Date')
 df_sorted['com_rev']=df_sorted['Price Each'].cumsum().round(2)
 st.title('what is cmulatitave revenue from start data till and end ?')
 st.plotly_chart(px.line(df_sorted,x='Order Date',y='com_rev'))
+
